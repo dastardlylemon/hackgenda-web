20abeaf8ce67fd1567ffd0508ef6c5446d6fa241
Daniel Duan <danielduan88@yahoo.com>
2014-02-15 05:34:14 -0500

    admin

--- config/secrets.js
+++ config/secrets.js
@@ -1,26 +1,12 @@
 module.exports = {
-  db: 'localhost',
+  db: process.env.MONGOHQ_URL || 'localhost',
 
   localAuth: true,
   sessionSecret: "Your Session Secret goes here",
 
-  mailgun: {
-    login: 'Your Mailgun SMTP Username',
-    password: 'Your Mailgun SMTP Password'
-  },
-
   sendgrid: {
     user: 'Your SendGrid Username',
     password: 'Your SendGrid Password'
-  },
-
-  nyt: {
-    key: 'Your New York Times API Key'
-  },
-
-  lastfm: {
-    api_key: 'Your API Key',
-    secret: 'Your API Secret'
   },
 
   facebookAuth: true,
@@ -47,46 +33,8 @@ module.exports = {
     passReqToCallback: true
   },
 
-  googleAuth: false,
-  google: {
-    clientID: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
-    callbackURL: '/auth/google/callback',
-    passReqToCallback: true
-  },
-
-  steam: {
-    apiKey: 'Your Steam API Key'
-  },
-
   twilio: {
     sid: 'Your Account SID',
     token: 'Your Auth Token'
   },
-
-  tumblr: {
-    consumerKey: 'Your Consumer Key',
-    consumerSecret: 'Your Consumer Secret',
-    callbackURL: '/auth/tumblr/callback'
-  },
-
-  foursquare: {
-    clientId: 'Your Client ID',
-    clientSecret: 'Your Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/foursquare/callback'
-  },
-
-  venmo: {
-    clientId: 'Your Venmo Client ID',
-    clientSecret: 'Your Venmo Client Secret',
-    redirectUrl: 'http://localhost:3000/auth/venmo/callback'
-  },
-
-  paypal: {
-    host: 'api.sandbox.paypal.com', // or api.paypal.com
-    client_id: 'Your Client ID',
-    client_secret: 'Your Client Secret',
-    returnUrl: 'http://localhost:3000/api/paypal/success',
-    cancelUrl: 'http://localhost:3000/api/paypal/cancel'
-  }
 };
