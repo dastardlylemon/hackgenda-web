7d64ee30f252b059ebc2b54d9e9c4a4549678c45
Daniel Duan <danielduan88@yahoo.com>
2014-02-15 16:37:43 -0500

    add schedule

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedi
 
 //app.get('/admin/adminUpdates', apiController.adminUpdates);
 app.get('/admin/scheduleUpdates', adminController.scheduleUpdates);
-app.post('/admin/scheduleUpdates', adminController.addScheduleUpdates);
+app.post('/admin/scheduleUpdates', adminController.postScheduleUpdates);
 //app.get('/admin/sponsorUpdates', apiController.sponsorUpdates);
 //app.get('/admin/awardUpdates', apiController.awardUpdates);
 //app.get('/admin/socialUpdates', apiController.socialUpdates);
