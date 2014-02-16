fad90d33f27d7a5517f6f60d1f0356ed6d4ecf98
dastardlylemon <jwu@efunda.com>
2014-02-15 20:39:04 -0500

    add firechat

--- controllers/web.js
+++ controllers/web.js
@@ -1,8 +1,27 @@
 var Schedule = require('../models/Schedule');
+var FirebaseTokenGenerator = require("firebase-token-generator");
 
 exports.getChat = function(req, res) {
+  if (req.user == null) {
+    req.flash('errors', { msg: 'Please sign in first.' });
+    res.redirect('/login');
+  }
+  var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET);
+  if (req.user.facebook) {
+    var id = req.user.facebook;
+    var token = tokenGenerator.createToken({id: id});
+  } else if (req.user.twitter) {
+    var id = req.user.twitter;
+    var token = tokenGenerator.createToken({id: id});
+  } else {
+    req.flash('errors', { msg: 'Please sign in with Twitter or Facebook.' });
+    res.redirect('/login ');
+  }
   res.render('chat', {
-    title: 'Chat'
+    title: 'Chat',
+    token: token,
+    user: req.user,
+    id: id
   });
 };
 
